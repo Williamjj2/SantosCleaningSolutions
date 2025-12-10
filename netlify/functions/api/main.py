119a4d072d9ef7dace044d30255b05928c9053cd
William Jesus <williamjj2@gmail.com>
2025-12-09 19:59:49 -0500

    Refactor: Structure function as a self-contained api package with its own requirements

--- netlify/functions/api/main.py
+++ netlify/functions/api/main.py
@@ -1,7 +1,10 @@
 import sys
 import os
 from mangum import Mangum
-from server import app
+try:
+    from .server import app
+except ImportError:
+    from server import app
 
 # Configura o handler para o Netlify Functions (AWS Lambda)
 handler = Mangum(app)
