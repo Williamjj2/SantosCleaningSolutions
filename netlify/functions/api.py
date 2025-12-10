9fa96b581d15b11813dcefd001f7e8eb15e11411
William Jesus <williamjj2@gmail.com>
2025-12-09 20:06:47 -0500

    Fix: Revert to flat structure in netlify/functions root to fix 404

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,10 +1,7 @@
 import sys
 import os
 from mangum import Mangum
-try:
-    from .server import app
-except ImportError:
-    from server import app
+from server import app
 
 # Configura o handler para o Netlify Functions (AWS Lambda)
 handler = Mangum(app)
